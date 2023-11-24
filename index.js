c8e78d8f26c4aadfd663dbf17854371fb99379da
shimu-7 <shamima19shimu@gmail.com>
2023-11-24 23:10:03 +0600

    api created to make user admin

--- index.js
+++ index.js
@@ -29,9 +29,51 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const userCollection = client.db("bistroDB").collection("users");
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("carts");
+
+
+        // user related
+        app.get('/users', async(req,res)=>{
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async(req,res)=>{
+            const user = req.body;
+            // checking user whether exists in db or not
+            const query = {email: user.email};
+            const existingUser = await userCollection.findOne(query);
+            if(existingUser){
+                return res.send({message: "user already exist" , insertedId: null})
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async(req,res)=>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updatedDoc={
+                $set:{
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateMany(filter, updatedDoc);
+            res.send(result);
+            
+        })
+
+        app.delete('/users/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query ={_id: new ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // menu related
 
         app.get('/menu', async(req,res)=>{
             const result =await menuCollection.find().toArray();
@@ -42,7 +84,7 @@ async function run() {
             res.send(result)
         })
 
-
+        // cart api
         app.get('/carts', async(req,res)=>{
             const email = req.query.email;
             const query = {email: email};
