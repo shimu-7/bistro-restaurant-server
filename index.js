69155af913421899213213fd3ca1f310407a1540
shimu-7 <shamima19shimu@gmail.com>
2023-11-24 10:52:28 +0600

    cart get and post api created

--- index.js
+++ index.js
@@ -31,10 +31,26 @@ async function run() {
 
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewCollection = client.db("bistroDB").collection("reviews");
+        const cartCollection = client.db("bistroDB").collection("carts");
 
         app.get('/menu', async(req,res)=>{
             const result =await menuCollection.find().toArray();
             res.send(result)
+        })
+        app.get('/reviews', async(req,res)=>{
+            const result =await reviewCollection.find().toArray();
+            res.send(result)
+        })
+
+
+        app.get('/carts', async(req,res)=>{
+            const result = await cartCollection.find().toArray();
+            res.send(result);
+        })
+        app.post('/carts', async(req,res)=>{
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
         })
 
 
