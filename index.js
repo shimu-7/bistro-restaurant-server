067c0b0f6d57a68bc0f6d380c2cdcc20b7e209db
shimu-7 <shamima19shimu@gmail.com>
2023-11-25 03:07:32 +0600

    middleware created to verify admin role

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -34,71 +35,122 @@ async function run() {
         const reviewCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("carts");
 
+        //jwt related
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            //token generate
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+            res.send({ token })
+        })
+
+        //middleware to verify token
+        const verifyToken = (req, res, next) => {
+            console.log('get token form verifyToken', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'Unauthorized Access' })
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'Unauthorized Access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        // verify user as admin
+        const verifyAdmin = async(req,res,next)=>{
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role==='admin';
+            if(!isAdmin){
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+            next();
+        }
+
 
         // user related
-        app.get('/users', async(req,res)=>{
+        app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
+
             const result = await userCollection.find().toArray();
             res.send(result);
         })
 
-        app.post('/users', async(req,res)=>{
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin })
+        })
+
+        app.post('/users', async (req, res) => {
             const user = req.body;
             // checking user whether exists in db or not
-            const query = {email: user.email};
+            const query = { email: user.email };
             const existingUser = await userCollection.findOne(query);
-            if(existingUser){
-                return res.send({message: "user already exist" , insertedId: null})
+            if (existingUser) {
+                return res.send({ message: "user already exist", insertedId: null })
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
 
-        app.patch('/users/admin/:id', async(req,res)=>{
+        app.patch('/users/admin/:id',verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
-            const updatedDoc={
-                $set:{
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
                     role: 'admin'
                 }
             }
             const result = await userCollection.updateMany(filter, updatedDoc);
             res.send(result);
-            
+
         })
 
-        app.delete('/users/:id', async(req,res)=>{
+        app.delete('/users/:id',verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const query ={_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
         // menu related
 
-        app.get('/menu', async(req,res)=>{
-            const result =await menuCollection.find().toArray();
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
             res.send(result)
         })
-        app.get('/reviews', async(req,res)=>{
-            const result =await reviewCollection.find().toArray();
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
             res.send(result)
         })
 
         // cart api
-        app.get('/carts', async(req,res)=>{
+        app.get('/carts', async (req, res) => {
             const email = req.query.email;
-            const query = {email: email};
+            const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
         })
-        app.post('/carts', async(req,res)=>{
+        app.post('/carts', async (req, res) => {
             const cartItem = req.body;
             const result = await cartCollection.insertOne(cartItem);
             res.send(result);
         })
-        app.delete('/carts/:id', async(req,res)=>{
+        app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id;
-            const query ={_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
