067c165e291a189ad3633bc59456eb11998de49d
shimu-7 <shamima19shimu@gmail.com>
2023-11-24 16:33:22 +0600

    delete api to remove item form cart

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -44,12 +44,20 @@ async function run() {
 
 
         app.get('/carts', async(req,res)=>{
-            const result = await cartCollection.find().toArray();
+            const email = req.query.email;
+            const query = {email: email};
+            const result = await cartCollection.find(query).toArray();
             res.send(result);
         })
         app.post('/carts', async(req,res)=>{
             const cartItem = req.body;
             const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
+        })
+        app.delete('/carts/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query ={_id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
